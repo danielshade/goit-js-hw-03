1a30db138cc4affc726f63c328b577a248391768
danielshade <113280712+danielshade@users.noreply.github.com>
2024-09-09 17:17:51 +0300

    update index html file

---
+++ js/task-1.js
@@ -0,0 +1,13 @@
+"use strict ";
+
+function slugify(title) {
+  let titleArray = title.split(" ");
+  let titleString = titleArray.join("-");
+  let slug = titleString.toLowerCase();
+  return slug;
+}
+
+console.log(slugify("Arrays for beginners")); // "arrays-for-beginners"
+console.log(slugify("English for developer")); // "english-for-developer"
+console.log(slugify("Ten secrets of JavaScript")); // "ten-secrets-of-javascript"
+console.log(slugify("How to become a JUNIOR developer in TWO WEEKS")); // "how-to-become-a-junior-developer-in-two-weeks"
